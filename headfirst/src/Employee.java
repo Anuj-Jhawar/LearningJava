e00698fbb8a7c53cba354a161ea236750e230a26
Jhawar Anuj Rajkumar <jarajkumar@tekion.com>
2023-01-20 12:16:58 +0530

    Added one file named Headfirst Java

---
+++ headfirst/src/Employee.java
@@ -0,0 +1,16 @@
+class Employee{
+    String Name;
+    int  ID;
+    String Position;
+
+    void ShowName(){
+        System.out.println(this.Name);
+    }
+    void ShowID(){
+        System.out.println(this.ID);
+    }
+    void ShowPosition(){
+        System.out.println(this.Position);
+    }
+}
+
---
+++ headfirst/src/EmployeeTest.java
@@ -0,0 +1,17 @@
+
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee Employee1 = new Employee();
+        Employee1.Name = "Anuj";
+        Employee1.ID = 3473;
+        Employee1.Position = "Intern";
+        //System.out.println(Employee1.ID);
+        Employee1.ShowName();
+        Employee1.ShowID();
+        Employee1.ShowPosition();
+
+
+    }
+}
+
